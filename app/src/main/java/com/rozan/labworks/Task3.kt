dd2012edc26e5caaafbcabd0fd0dcbb11408a358
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2020-12-09 19:10:15 +0545

    Task3(Post Card) using LinearLayout added.

---
+++ app/src/main/java/com/rozan/labworks/Task3.kt
@@ -0,0 +1,11 @@
+package com.rozan.labworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Task3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task3)
+    }
+}
